3ed594a8e1b9129bf6dee48b335aaeca26231afe
fitri <fitrimuslikhah7@gmail.com>
2024-05-25 22:58:59 +0700

    test_run basic-programming-part4

--- problem2/main.py
+++ problem2/main.py
@@ -1,10 +1,18 @@
 def draw_xyz(N):
     pattern = ""
-    for i in range(N):
-        for j in range(N):
-            pattern += chr(88 + (i + j) % 3) + " "
-        pattern += "\n"
-    return pattern
+    if number <= 1 :
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0 :
+        return False
+    elif number <= 5:
+        return True
+    else :
+        for i in range(5,9):
+            if number % i == 0:
+                return False
+            return True
 
 if __name__ == '__main__':
     print(draw_xyz(3))
